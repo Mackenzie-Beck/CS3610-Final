2601db00f32a210abc228566e8af05b9a2c63300
M.Beck <mackbeckgames23@gmail.com>
2024-12-17 14:09:09 -0700

    final comments

--- Client.py
+++ Client.py
@@ -2,22 +2,27 @@ from WebApp import *
 from concreteImplementations import *
 
 
+
+# this is the client code which uses the WebApp class to create the web applications.
+
+# we create a blog with the light theme.
 blog = Blog(LightTheme())
 blog.create_post("Hello", "This is a post")
 blog.post_message("This is a message")
 
+# we change the theme of the blog to dark.
 blog.set_implementation(DarkTheme())
 blog.create_post("Hello", "This is a post")
 blog.post_message("This is a message")
 
 print("\n")
 
-
-
+# we create a store with the dark theme.
 store = Store(DarkTheme())
 store.create_product("Apple", 1.99)
 store.post_message("This is a message")
 
+# we change the theme of the store to light.
 store.set_implementation(LightTheme())
 store.create_product("Apple", 1.99)
 store.post_message("This is a message")
@@ -25,11 +30,12 @@ store.post_message("This is a message")
 
 print("\n")
 
+# we create a news with the light theme.
 news = News(LightTheme())
 news.create_article("Hello", "This is a article")
 news.post_message("This is a message")
 
+# we change the theme of the news to dark.
 news.set_implementation(DarkTheme())
 news.create_article("Hello", "This is a article")
 news.post_message("This is a message")
-    
